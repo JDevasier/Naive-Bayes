af60279b8f4594f38a396b8a5b45b06abe058b3f
Jacob Devasier <jdevasier12@gmail.com>
2018-09-20 19:45:02 -0500

    Finished
    To use the naive bayes classifier, import the function naive_bayes and fun it with the args naive_bayes(<training_file>, <testing_file>)

--- naive_bayes.py
+++ naive_bayes.py
@@ -3,18 +3,9 @@ import sys
 import numpy as np
 import math
 
-# args: [<training_file>, <test_file>]
-args = sys.argv
-training_file = args[1]
-testing_file = args[2]
-
 training_data = []
 num_observations = 0
 num_classes = 0
-
-
-def main():
-    naive_bayes(training_file, testing_file)
 
 
 def naive_bayes(train_file, test_file):
@@ -33,98 +24,53 @@ def naive_bayes(train_file, test_file):
     test_data = loadFile(test_file)
     gaussians = getAllClassGaussians(classes)
 
-    
+    correct = 0
+    for i in range(np.shape(test_data)[0]):
+        acc = 0
+        obs = test_data[i]
+        calc = calculateProbability(classes, gaussians, obs[:-1])
+        if (calc[0] == int(obs[-1])):
+            correct += 1
+            acc = 1
 
-<<<<<<< HEAD:naive_bayes.py
-    print("Classification Accuracy = {:4.6f}".format(acc))
-=======
-    calculateProbability(classes, gaussians, test_data[0])
->>>>>>> 7a1dbd6bd25cde3b0cee9edc83af4540db84310b:niave_bayes.py
+        print("ID = {:5.0f}, Predicted = {:3.0f}, Probability = {:0.4f}, True = {:3.0f}, Accuracy = {:4.2f}".format(
+            i+1, calc[0], calc[1], obs[-1], acc))
 
-    #predictions = makePredictions(classes, gaussians, test_data)
-    #acc = checkAccuracy(test_data[:, -1], predictions)
+    print("Classification Accuracy = {:6.4f}".format(
+        correct / np.shape(test_data)[0]))
 
-    #print("Classification Accuracy = ", acc)
 
 def calculateProbability(classes, gaussians, obs):
     denom = 0
     for cl_num in classes.keys():
-        denom += probGivenClass(obs, cl_num, gaussians) * classFrequency(classes, cl_num)
+        denom += probGivenClass(obs, cl_num, gaussians) * \
+            classFrequency(classes, cl_num)
 
     max_prob = -1
     max_class = 0
     for cl_num in classes.keys():
-        prob = probGivenClass(obs, cl_num, gaussians) * classFrequency(classes, cl_num)
+        prob = probGivenClass(obs, cl_num, gaussians) * \
+            classFrequency(classes, cl_num)
         prob = prob / denom
         if (prob > max_prob):
             max_prob = prob
             max_class = cl_num
+    return (max_class, max_prob)
+
 
 def classFrequency(classes, cl_num):
     return np.shape(classes[cl_num])[0] / num_observations
 
+
 def probGivenClass(obs, cl_num, gaussians):
     prob_density = 1
     for col in range(len(gaussians[cl_num])):
-        dens = calculate(obs[col], gaussians[cl_num][col][0], gaussians[cl_num][col][1])
+        dens = calculate(obs[col], gaussians[cl_num][col]
+                         [0], gaussians[cl_num][col][1])
         prob_density *= dens
     return prob_density
 
-def checkAccuracy(real, predictions):
-    right = 0
-    for i in range(len(real)):
-        if (real[i] == predictions[i]):
-            right += 1
-    return right / len(real)
-
-#todo: divide each probability by probability of an observation in each class (SUM RULE)
-def makePredictions(classes, gaussians, test_data):
-    observations = []
-    for i in range(len(test_data)):
-        obs = test_data[i]
-        real = obs[-1]
-        pred = calcClass(classes, gaussians, obs[:-1])
-        observations.append(pred[1])
-        acc = 0
-        if (pred[1] == real):
-            acc = 1
-
-        print("ID = {:.0f}, Predicted = {:.0f}, Probability = {:e}, True = {:.0f}, Accuracy = {:.0f}".format(
-            i+1, pred[1], pred[0][pred[1]], real, acc))
-
-    return observations
-
-
-def calcClass(classes, class_gaussians, observation):
-    prob = {}
-
-    den = 1
-    for class_num in list(class_gaussians.keys()):
-        den *= probabilityClass(classes, class_num)
-
-    for class_num, gaussians in class_gaussians.items():
-        prob[class_num] = 1
-        for i in range(len(gaussians)):
-            mean, std = gaussians[i]
-            x = observation[i]
-            calc = calculate(x, mean, std)
-            # if (calc > 1):
-            #print(class_num, i, x, gaussians[i], calc)
-            prob[class_num] *= calc
-        prob[class_num] *= probabilityClass(classes, class_num) / den
-
-    amax = argmax(prob)
-    return (prob, amax)
-
-
-def argmax(d):
-    arg = -1
-    highest = -1
-    for k, v in d.items():
-        if (v >= highest):
-            highest = v
-            arg = k
-    return arg
+# todo: divide each probability by probability of an observation in each class (SUM RULE)
 
 
 def probabilityClass(classes, class_num):
@@ -149,7 +95,7 @@ def getClassGaussians(classes, class_num):
         gauss = getGaussian(classes[class_num], dimension)
         class_gaussians.append(gauss)
         # print out here so i dont have to rewrite the code :)
-        print("Class {:.0f}, attribute {:.0f}, mean = {:f}, std = {:f}".format(
+        print("Class {:.0f}, attribute {:.0f}, mean = {:0.2f}, std = {:0.2f}".format(
             class_num, dimension, gauss[0], gauss[1]))
     return class_gaussians
 
@@ -185,6 +131,3 @@ def loadFile(file_name):
 
         data = np.asarray(data)
         return data
-
-
-main()
